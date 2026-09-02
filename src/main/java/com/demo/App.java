43644ec6a74a9449eaa8ec99572807c6d766252c
Fehizoro <ralsondrina@gmail.com>
2026-09-02 10:22:06 +0200

    Transformation en projet de gestion de bibliotheque

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -1,11 +1,23 @@
 package com.demo;
 
 public class App {
-    public static String greet() {
-        return "Hello Jenkins Pipeline!";
-    }
-
     public static void main(String[] args) {
-        System.out.println(greet());
+        Library library = new Library();
+
+        library.addBook(new Book("Le Petit Prince", "Antoine de Saint-Exupéry"));
+        library.addBook(new Book("1984", "George Orwell"));
+        library.addBook(new Book("Clean Code", "Robert C. Martin"));
+
+        System.out.println("=== Bibliothèque ===");
+        System.out.println("Nombre total de livres : " + library.getBookCount());
+        System.out.println("Livres disponibles : " + library.getAvailableCount());
+
+        System.out.println("\nEmprunt de '1984'...");
+        library.borrowBook("1984");
+        System.out.println("Livres disponibles : " + library.getAvailableCount());
+
+        System.out.println("\nRetour de '1984'...");
+        library.returnBook("1984");
+        System.out.println("Livres disponibles : " + library.getAvailableCount());
     }
 }
